53415ad23b49598fc128ea042c9f1ae3f7785e3f
FATBOY123456789 <ytzjimbuisness@gmail.com>
2024-10-10 17:49:31 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+class Parrot:
+    species='bird'
+    def __init__(self,name,age):
+        self.name= name
+        self.age= age
+birg=Parrot('Grakanawaw','8')
+print('The birds name is: ',birg.name,'and the birds age is: ',birg.age,'months old')
